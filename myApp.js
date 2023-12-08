7f7de6077cfdb07246d00f56ed3d4a600dfb5fff
camoltoni <cesarmoltoni@gmail.com>
2023-12-08 17:49:03 +0000

    Assignment 6/12 from freecodecamp "Back end development and API's"

--- myApp.js
+++ myApp.js
@@ -1,39 +1,18 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+// console.log("Hello World");
+app.get("/", function (req, res) {
+  let viewsPath = __dirname + "/views/index.html";
+  res.sendFile(viewsPath);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+let publicPath = __dirname + "/public";
+app.use("/public", express.static(publicPath));
+app.get("/json", function (req, res) {
+  let o = {message:"Hello json"}
+  if(process.env.MESSAGE_STYLE == "uppercase") {
+    o["message"] = "HELLO JSON"
+  }
+  res.json(o);
+});
+module.exports = app;
